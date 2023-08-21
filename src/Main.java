872742cf6198df69315408699282d93b73855c91
Tandon245 <tandonabhishek245@gmail.com>
2023-08-21 10:13:51 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        int attempts = 1; //in very first attempt will be zero
+        Random rand = new Random();
+        int randInt = rand.nextInt(100) + 1; //this will create random number. Every time the program will run create random number between 1-100
+        System.out.println(randInt);
+        while (true) {
+
+            int n = sc.nextInt();
+
+            if(n==randInt){
+                System.out.println("You Have Guessed The Number in "+attempts+" attempts");
+                break;
+            }
+            if(n<randInt){
+                System.out.println("Please Enter a Higher Number");
+            }
+            if(randInt<n){
+                System.out.println("Please Enter a Lower Number");
+            }
+            attempts++;
+        }
+
+
     }
 }
